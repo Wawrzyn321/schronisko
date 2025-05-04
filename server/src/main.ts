f1c438d5d3597147ed205a1ce0db8e32877ed414
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2025-05-04 13:47:17 +0200

    Fix path

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,7 +11,7 @@ import * as Sentry from '@sentry/node';
 
 import * as bodyParser from 'body-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { MAX_REQUEST_SIZE, PORT } from 'config';
+import { MAX_REQUEST_SIZE, PORT } from './config';
 
 const DEV = process.env.NODE_ENV !== 'production';
 
